cfa6b4c0c0eed9b81b0041048e2aa7f9b1c81c38
Victor Costan <costan@gmail.com>
2015-12-13 00:06:15 -0500

    Support for querying if a flash page is blank.

---
+++ lib/sphero_pwn/commands/is_page_blank.rb
@@ -0,0 +1,35 @@
+# Asks the robot's bootloader if a flash page is blank.
+#
+# This command is only valid when the robot is executing the bootloader.
+class SpheroPwn::Commands::IsPageBlank < SpheroPwn::Command
+  # @param {Number} page_number the flash page that the command will ask the
+  #   robot to look at
+  def initialize(page_number)
+    if page_number > 255
+      raise ArgumentError, "Page number #{page_number} exceeds 255"
+    end
+    super 0x01, 0x05, [page_number]
+  end
+
+  # @see {SpheroPwn::Command#response_class}
+  def response_class
+    SpheroPwn::Commands::IsPageBlank::Response
+  end
+end
+
+# The response to a boot main application command.
+class SpheroPwn::Commands::IsPageBlank::Response < SpheroPwn::Response
+  # @return {Boolean} true if the flash memory page is blank
+  attr_reader :is_blank
+
+  # @see {SpheroPwn::Response#initialize}
+  def initialize(code_byte, sequence_byte, data_bytes)
+    super
+
+    if code == :ok
+      @is_blank = data_bytes[0] > 0
+    else
+      @is_blank = nil
+    end
+  end
+end
---
+++ test/commands/is_page_blank_test.rb
@@ -0,0 +1,63 @@
+require_relative '../helper.rb'
+
+describe SpheroPwn::Commands::IsPageBlank do
+  it 'stringifies correctly' do
+    is_page_blank = SpheroPwn::Commands::IsPageBlank.new 0x67
+    sequence = 0x52
+
+    assert_equal [0xFF, 0xFF, 0x01, 0x05, 0x52, 0x02, 0x67, 0x3E],
+                 is_page_blank.to_bytes(sequence).unpack('C*')
+  end
+
+  it 'parses a yes response correctly' do
+    response = SpheroPwn::Commands::IsPageBlank::Response.new 0x00, 0x00,
+        [0x01]
+
+    assert_equal :ok, response.code
+    assert_equal true, response.is_blank
+  end
+
+  it 'parses a no response correctly' do
+    response = SpheroPwn::Commands::IsPageBlank::Response.new 0x00, 0x00,
+        [0x00]
+
+    assert_equal :ok, response.code
+    assert_equal false, response.is_blank
+  end
+
+  it 'does not crash when parsing an error response' do
+    response = SpheroPwn::Commands::IsPageBlank::Response.new 0x32, 0x00,
+        [0x01]
+
+    assert_equal :bad_page, response.code
+    assert_equal nil, response.is_blank
+  end
+
+  describe 'when sent to the robot' do
+    before do
+      @session = new_test_session :is_page_blank
+      @session.send_command SpheroPwn::Commands::EnterBootloader.new
+      response = @session.recv_until_response
+      unless response.code == :ok
+        raise RuntimeError, 'Could not jump into bootloader'
+      end
+    end
+    after do
+      @session.send_command SpheroPwn::Commands::BootMainApp.new
+      response = @session.recv_until_response
+      unless response.code == :ok
+        raise RuntimeError, 'Could not boot back into the main application'
+      end
+
+      @session.close
+    end
+
+    it 'gets an ok response' do
+      @session.send_command SpheroPwn::Commands::IsPageBlank.new(16)
+      response = @session.recv_until_response
+      assert_kind_of SpheroPwn::Commands::IsPageBlank::Response, response
+      assert_equal :ok, response.code
+    end
+  end
+end
+
